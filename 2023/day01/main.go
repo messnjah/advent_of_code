e70dd5df8ba9f372f198d6792b738dac64d14558
messnjah <121194732+messnjah@users.noreply.github.com>
2023-12-03 14:55:53 -0600

    day1 solution

--- 2023/day01/main.go
+++ 2023/day01/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"bufio"
-	"errors"
 	"fmt"
 	"log"
 	"os"
@@ -11,41 +10,32 @@ import (
 	"strings"
 )
 
-func reverse_string(s string) string {
-	chars := []rune(s)
-	for idx, jump := 0, len(chars)-1; idx < jump; idx, jump = idx+1, jump-1 {
-		chars[idx], chars[jump] = chars[jump], chars[idx]
-	}
-	return string(chars)
-}
+func part1(s string) (int, error) {
+	number_slice := []string{"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"}
+	idx_locations := [][]string{}
 
-func find_int(s string) (int, string, error) {
-	//num_idx :=
-	// Iterate over each character in the line
-	for idx, char := range s {
-		// Check if the character can be converted to an integer
-		if _, err := strconv.Atoi(string(char)); err == nil {
-			return idx, string(char), nil
-		}
-	}
-	return 0, "", errors.New("No int found")
-}
-
-func find_int_name(s string) {
-	valid_num := [9]string{"one", "two", "three", "four", "five", "six", "seven", "eight", "nine"}
-	for _, number := range valid_num {
-		if result := strings.Contains(s, number); result == true {
-			fmt.Println(strings.Index(s, number), strings.Index(s, number)+len(number)-1, number)
-			// TODO: Find the index locations of the int and word int. Determine which shows up first and grab that number
-			// TODO: Create forward array for first number and backward array for last number
+	for _, number := range number_slice {
+		if found_number := strings.Contains(s, number); found_number == true {
+			results := []string{}
+			results = append(results, strconv.Itoa(strings.Index(s, number)), number)
+			idx_locations = append(idx_locations, [][]string{results}...)
+			last := []string{}
+			last = append(last, strconv.Itoa(strings.LastIndex(s, number)), number)
+			idx_locations = append(idx_locations, [][]string{last}...)
 		}
 
 	}
-
+	sort.Slice(idx_locations, func(i, j int) bool {
+		a, _ := strconv.Atoi(idx_locations[i][0])
+		b, _ := strconv.Atoi(idx_locations[j][0])
+		return a < b
+	})
+	return strconv.Atoi(idx_locations[0][1] + idx_locations[len(idx_locations)-1][1])
 }
 
 func part2(s string) (int, error) {
 	number_map := map[string]string{
+		"zero":  "0",
 		"one":   "1",
 		"two":   "2",
 		"three": "3",
@@ -59,13 +49,19 @@ func part2(s string) (int, error) {
 
 	for word, number := range number_map {
 		if found_word := strings.Contains(s, word); found_word == true {
-			results := []string{strconv.Itoa(strings.Index(s, word)), number} // Get index and number for the word
+			results := []string{strconv.Itoa(strings.Index(s, word)), number}
 			idx_locations = append(idx_locations, [][]string{results}...)
+			last := []string{}
+			last = append(last, strconv.Itoa(strings.LastIndex(s, word)), number)
+			idx_locations = append(idx_locations, [][]string{last}...)
 		}
 		if found_number := strings.Contains(s, number); found_number == true {
-			results := []string{} // Get index and number for the word
+			results := []string{}
 			results = append(results, strconv.Itoa(strings.Index(s, number)), number)
 			idx_locations = append(idx_locations, [][]string{results}...)
+			last := []string{}
+			last = append(last, strconv.Itoa(strings.LastIndex(s, number)), number)
+			idx_locations = append(idx_locations, [][]string{last}...)
 		}
 
 	}
@@ -74,34 +70,25 @@ func part2(s string) (int, error) {
 		b, _ := strconv.Atoi(idx_locations[j][0])
 		return a < b
 	})
-	fmt.Println(idx_locations[0][1] + idx_locations[len(idx_locations)-1][1])
-	fmt.Println(idx_locations)
 	return strconv.Atoi(idx_locations[0][1] + idx_locations[len(idx_locations)-1][1])
 }
 
 func main() {
-	file, err := os.Open("test.txt")
+	file, err := os.Open("input.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer file.Close()
 
 	file_scanner := bufio.NewScanner(file)
-	// file_scanner.Split(bufio.ScanLines)
 	part1_total := 0
 	part2_total := 0
-	// Scan the file line by line
 	for file_scanner.Scan() {
-		_, first_num, _ := find_int(file_scanner.Text())
-		_, second_num, _ := find_int(reverse_string(file_scanner.Text()))
-		result, _ := strconv.Atoi(first_num + second_num)
-		part1_total = part1_total + result
-		//fmt.Println(result)
-		//find_int_name(file_scanner.Text())
+		part1_num, _ := part1(file_scanner.Text())
+		part1_total += part1_num
 		part2_num, _ := part2(file_scanner.Text())
-		part2_total = part2_total + part2_num
+		part2_total += part2_num
 	}
-	fmt.Println(part1_total)
-	fmt.Println(part2_total)
-
+	fmt.Printf("Part 1 total: "+"%d\n", part1_total)
+	fmt.Printf("Part 2 total: "+"%d\n", part2_total)
 }
