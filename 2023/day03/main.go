bfed2f10dbc74aa1c312b0d17fef8794ae29bdcd
messnjah <121194732+messnjah@users.noreply.github.com>
2023-12-06 06:50:34 -0600

    day3

--- 2023/day03/main.go
+++ 2023/day03/main.go
@@ -11,6 +11,10 @@ type Point struct {
 	X, Y int
 }
 
+func (this Point) checkPoint(d Point) Point {
+	return Point{this.X + d.X, this.Y + d.Y}
+}
+
 func part1(file []byte) {
 	symbols := map[Point]string{}
 	parts := map[Point]string{}
@@ -19,7 +23,7 @@ func part1(file []byte) {
 			if r != 46 && (r < 48 || r > 57) {
 				symbols[Point{x, y}] = string(r)
 			}
-			if  r > 47 && r < 58 {
+			if r > 47 && r < 58 {
 				parts[Point{x, y}] = string(r)
 			}
 		}
@@ -30,17 +34,31 @@ func part1(file []byte) {
 	directions := []Point{
 		{-1, -1}, {-1, 0}, {-1, 1}, {0, -1}, {0, 1}, {1, -1}, {1, 0}, {1, 1},
 	}
+	found := map[Point]string{}
 	//TODO: For each symbol coordinate, search around and grab coordinate
 	// if that coordinate exists in the parts map
 	for k, _ := range symbols {
-		fmt.Println(k)
+		//fmt.Printf("Symbol Point: %v:\n", k)
 		for _, d := range directions {
-			if _, exists := symbols[d] {
-				fmt.Println()
+			check := Point{k.X, k.Y}.checkPoint(d)
+
+			if s, exists := parts[check]; exists {
+				found[check] = s
 			}
 		}
 	}
-	
+	fmt.Println(found)
+	y := 0
+	x := -1
+	for k, _ := range found {
+		fmt.Println(k)
+		//Walk Left
+
+		//for x := -1; test := parts[k.checkPoint(Point{x,y})]; x--
+		check := k.checkPoint(Point{x, y})
+		fmt.Println(check)
+		x--
+	}
 
 }
 
