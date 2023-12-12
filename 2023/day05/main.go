a2c7112dc151f62053667fe706c18ca5adaf08e6
messnjah <121194732+messnjah@users.noreply.github.com>
2023-12-12 13:50:11 -0600

    testing optimization

--- 2023/day05/main.go
+++ 2023/day05/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"sort"
 	"strconv"
 	"strings"
 )
@@ -17,6 +18,18 @@ type Detail struct {
 }
 
 type Maps map[string]*[]Detail
+
+/* func (this Maps) Len() int {
+    return len(this)
+}
+
+func (this Maps) Less(i, j int) bool {
+    return this[i].src > this[j].src
+}
+
+func (e employeeList) Swap(i, j int) {
+    e[i], e[j] = e[j], e[i]
+} */
 
 func readLines(filename string) []string {
 	file, err := os.ReadFile(filename)
@@ -53,6 +66,12 @@ func processFile(lines []string) (Maps, []int) {
 			rows = append(rows, detail)
 		}
 		alm[mapName] = &rows
+
+		//P2opt sorting slices by src 0...99999
+		subMap := *alm[mapName]
+		sort.Slice(subMap, func(i, j int) bool {
+			return subMap[i].src < subMap[j].src
+		})
 	}
 
 	return alm, seeds
@@ -138,10 +157,56 @@ func part2(alm Maps, seeds []int) {
 	fmt.Println(location)
 }
 
+func part2Optimized(alm Maps, seeds []int) {
+	mapsList := [7]string {"seed-to-soil","soil-to-fertilizer","fertilizer-to-water","water-to-light","light-to-temperature","temperature-to-humidity","humidity-to-location" }
+	//location := 0
+	newSeeds := [][]int{}
+	for i:=0; i < len(seeds); i += 2 {
+		temp := []int{}
+		result := []int{seeds[i],seeds[i] + seeds[i+1] - 1}
+		temp = append(temp, result...)
+		newSeeds = append(newSeeds, temp)
+	}
+	fmt.Println(newSeeds)
+
+	for _, seedRange := range newSeeds {
+		for _, name := range mapsList {
+			subMap := alm[name]
+
+			for _, item := range *subMap {
+				if seedRange[0] >= item.src && seedRange[1] <= item.srcEnd {
+					//output = input + item.diff
+					//fmt.Printf("input: %v, %v: %v\n",input, name, output)
+					//found = true
+					fmt.Println(true)
+					fmt.Println(item)
+				} 
+			}
+	
+			/* if !found {
+				output = input
+				//fmt.Printf("input: %v, %v: %v\n",input, name, output)
+			}
+			found = false
+
+			if name == "humidity-to-location" && (location == 0 || output < location) {
+				location = output
+			}
+			input = output */	
+		}
+	}
+	
+	//sort.Sort(subMap)
+	fmt.Println(alm["soil-to-fertilizer"])
+
+}
+
+
 func main() {
 	file := "test.txt"
 	lines := readLines(file)
 	alm, seeds := processFile(lines)
-	part1(alm, seeds)
-	part2(alm, seeds)
+	//part1(alm, seeds)
+	//part2(alm, seeds)
+	part2Optimized(alm, seeds)
 }
